cb75eb345fc8bdeedbb9420cf9252668f7f1cd25
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-06-09 11:53:59 +0400

    update

--- src/components/AccordionCard.js
+++ src/components/AccordionCard.js
@@ -10,7 +10,7 @@ function AccordionCard() {
       itemNumber: 1,
       question: "How many team members can I invite?",
       answer:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. ",
     },
     {
       itemNumber: 2,
@@ -33,7 +33,7 @@ function AccordionCard() {
       itemNumber: 5,
       question: "Do you provide additional support?",
       answer:
-        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form",
+        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum",
     },
   ];
   const handleClick = (accordionNumber) => {
@@ -48,7 +48,7 @@ function AccordionCard() {
     } else {
       setActiveAccordionNumber(accordionNumber);
       const expanding = document.getElementById(accordionNumber);
-      const rotating = document.getElementById(accordionNumber+"arrow");
+      const rotating = document.getElementById(accordionNumber + "arrow");
       expanding.classList.add("expanded");
       rotating.classList.add("rotated");
     }
